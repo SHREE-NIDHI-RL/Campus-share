b31fc29efb7f5eea44b5f97792ed07d22065052b
SHREE-NIDHI-RL <shreenidhi.rl2024csecs@sece.ac.in>
2026-05-12 14:44:33 +0530

    added

--- campus-lend-borrow-main/tailwind.config.js
+++ campus-lend-borrow-main/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./app/**/*.{js,jsx}", "./src/**/*.{js,jsx}"],
@@ -73,5 +75,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
